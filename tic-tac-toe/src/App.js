b3a0e0012ca4f1b0f61a820502901421067040c5
Sam <sam.d.odonnell@gmail.com>
2019-05-27 13:14:07 -0700

    Add reset and resize buttons

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,10 +5,21 @@ import Board from './Board';
 function App() {
 
   const [size, setSize] = useState(3);
+  const [reset, setReset] = useState(0);
+
+  const handleReset = e => {
+    e.preventDefault(); 
+    setSize(3);
+    setReset(Math.random());
+  }
 
   return (
     <div className="App">
-      <Board size={size}/>
+      <Board key={reset} size={size} />
+      <form className="board-input">
+        <input type="number" min="3" value={size} onChange={e => setSize(e.target.value)}/>
+        <button onClick={handleReset}>Reset</button>
+      </form>
     </div>
   );
 }
--- tic-tac-toe/src/Board.jsx
+++ tic-tac-toe/src/Board.jsx
@@ -1,14 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import Square from './Square';
 
 const Board = ({size}) => {
 
-  const [currPlayer, setCurrPlayer] = useState("x");
-  const [grid, setGrid] = useState(Array(size).fill(Array(size)))
+  const createGrid = () => {
+    let grid = Array(size);
+    for (let i = 0; i < size; i++) {
+      grid[i] = Array(size).fill(null)
+    }
+    return grid;
+  }
 
-  // const gameOver = () => {
-  //   xWins() || oWins()
-  // }
+  const [currPlayer, setCurrPlayer] = useState("x");
+  const [grid, setGrid] = useState(createGrid());
+  const [won, setWon] = useState(false);
+
+  useLayoutEffect(() => {
+    setGrid(createGrid());
+  }, [size])
+
+  const winner = mark => {
+
+    // Row win
+    for (let row of grid) {
+      if (row.every(el => el === mark)) {
+        console.log('row')
+        return true
+      }
+    }
+
+    // Col win
+    for (let i = 0; i < size; i++) {
+      let col = [];
+      for (let j = 0; j < size; j++) {
+        if (grid[j][i] === mark) {
+          col.push(mark);
+        } else {
+          continue;
+        }
+      }
+      if (col.length === size) {
+        console.log('col')
+        return true
+      }
+    }
+
+    // Diagonal win 
+    let diag = [];
+    for (let row in grid) {
+      for (let col in grid) {
+        if (row === col && grid[row][col] === mark) {
+          diag.push(mark)
+        } 
+      }
+    }
+    if (diag.length === size) {
+      console.log('diag')
+      return true
+    }
+
+    return false
+  }
 
   const populateBoard = () => {
     let squares = [];
@@ -21,16 +73,19 @@ const Board = ({size}) => {
   }
 
   const handleClick = pos => {
+    if (won) return;
     let [x, y] = [pos[0], pos[1]];
     let updatedGrid = grid;
     updatedGrid[x][y] = currPlayer;
     setGrid(updatedGrid);
-    // return if gameOver();
+    if (winner('x') || winner('o')) {
+      setWon(true)
+    }
     currPlayer === "x" ? setCurrPlayer("o") : setCurrPlayer("x");
   }
 
   return (
-    <div className="board">
+    <div className="board" style={{gridTemplateColumns: `repeat(${size}, auto)`}}>
       {populateBoard()}
     </div>
   )
